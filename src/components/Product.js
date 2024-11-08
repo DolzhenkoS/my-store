a3fc974b64f70ee907bcbeb709d25114f57d68a3
DolzhenkoS <sergey_dolzhenko@gmail.com>
2024-11-08 17:09:12 +0300

    Изменена карточка продукта. Информация о доступности

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,13 +9,27 @@ function Product({ product, addToCart, openDetail, selectedProduct }) {
         openDetail(product);
     }
 
+    function info(product) {
+        if (product.maxq - product.rezerv > 0) {
+            return "Доступно: " + (product.maxq - product.rezerv) + " шт"
+        } else {
+            return "Не доступно"
+        }
+        // (product.maxq - product.rezerv>0)? "Доступно: "+product.maxq - product.rezerv:"Нет в наличии"
+    }
+
     return (
         <div key={product.article} className="product" >
             <img onClick={(e) => { onClick(e) }} src={imgUrl} alt={product.name}></img>
             <h2>{product.name}</h2>
+            <h3>{info(product)}</h3>
             <p>{product.price} руб</p>
-            <button onClick={() => { addToCart(product) }}>Добавить в заказ</button>
-            <div className='icon'>{product.maxq - product.rezerv}</div>
+            {((product.maxq - product.rezerv) > 0) ? (
+                <button  onClick={() => { addToCart(product) }}>Добавить в заказ</button>
+            ) : (
+                <button className='add-button' onClick={() => { addToCart(product) }}>Добавить в заказ</button>
+            )}
+            {/* <div className='icon'>{product.maxq - product.rezerv}</div> */}
 
         </div>
     )
